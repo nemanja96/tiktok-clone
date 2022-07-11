84d36f2cdfb576ae241f0670c64572a8a172999d
nemanja96 <nemanjaradivojevic1603@gmail.com>
2022-07-11 16:46:16 +0200

    Creating user info section

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,7 +10,7 @@ const Nav = () => {
   useEffect(() => {
     async function getData() {
       try {
-        const response = await axios.get('http://localhost:8001/tiktok/videos');
+        const response = await axios.get('https://peaceful-temple-46584.herokuapp.com/tiktok/videos');
         const videos = response.data;
         setVideos(videos);
       } catch (error) {
@@ -21,21 +21,19 @@ const Nav = () => {
     getData();
   }, [])
 
-  console.log(videos);
-
   return (
     <div className="h-screen grid place-items-center relative">
         <div className="absolute top-6 right-6 flex gap-10 items-center z-50">
             <div className="flex gap-4 items-center">
-                <h4 className="text-gray-500 text-md font-normal">Following</h4>
-                <h5 className="text-white font-bold text-lg">For You</h5>                
+                <h4 className="text-gray-300 text-sm font-bold">Following</h4>
+                <h5 className="text-white font-bold text-sm">For You</h5>                
             </div>
             <SearchOutlinedIcon sx={{ width: 28, height: 28 }} className="text-white" />
         </div>
         <div className="w-full h-screen relative overflow-scroll snap-mandatory snap-y z-10">
           {
-            videos.map(({ videoUrl, avatarUrl, likesNumber, commentsNumber, bookmarksNumber }) => (
-              <Video imgUrl={videoUrl} avatarUrl={avatarUrl} likes={likesNumber} comments={commentsNumber} bookmarks={bookmarksNumber} />
+            videos.map(({ videoUrl, avatarUrl, likesNumber, commentsNumber, bookmarksNumber, userName, description, hashtags }) => (
+              <Video imgUrl={videoUrl} avatarUrl={avatarUrl} likes={likesNumber} comments={commentsNumber} bookmarks={bookmarksNumber} userName={userName} description={description} hashtags={hashtags} />
             ))
           }
         </div>
--- src/components/Video.js
+++ src/components/Video.js
@@ -4,10 +4,10 @@ import Avatar from '@mui/material/Avatar';
 import SidebarNav from './SidebarNav';
 import AddIcon from '@mui/icons-material/Add';
 
-const Video = ({ imgUrl, avatarUrl, likes, comments, bookmarks }) => {
+const Video = ({ imgUrl, avatarUrl, likes, comments, bookmarks, userName, description, hashtags }) => {
   return (
-    <div className="flex w-full h-full max-h-[650px] bg-red-300 relative snap-start">
-        <img src={imgUrl} className="w-full h-screen" />
+    <div className="flex w-full h-full max-h-[800px] relative snap-start">
+        <div className="w-full h-screen" style={{ backgroundImage: `url(${imgUrl})`, backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat: "no-repeat" }} />
         <div className="absolute right-3 bottom-20 w-[50px] h-[400px] z-40 flex flex-col justify-end items-center">
           <div className="mb-6 flex relative justify-center">
             <Avatar src={avatarUrl} alt="Nemanja Radivojevic" sx={{ width: 35, height: 35 }} className="border border-white border-solid" />
@@ -16,6 +16,17 @@ const Video = ({ imgUrl, avatarUrl, likes, comments, bookmarks }) => {
             </span>
           </div>
           <SidebarNav avatarUrl={avatarUrl} likes={likes} comments={comments} bookmarks={bookmarks} />
+        </div>
+        <div className="absolute bottom-20 left-4">
+          <h3 className="text-white text-sm font-semibold">@{userName}</h3>
+          <p className="text-white text-sm mt-2 mb-1">{description}</p>
+          <div className="w-9/12 flex flex-wrap gap-x-2">
+            {
+              hashtags.map(hashtag => (
+                  <span className="text-white text-sm font-semibold">#{hashtag}</span>
+              ))
+            }
+          </div>
         </div>
     </div>
   )
